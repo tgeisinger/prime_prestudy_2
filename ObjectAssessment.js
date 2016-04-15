4a38a875004019c40916a3de782020c89bc2688d
tgeisinger <tttgeisinger@gmail.com>
2016-04-14 22:33:03 -0500

    first commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,11 @@
+var avengersMovie = {
+	name: "Avengers",
+	runtime: 143,
+	releaseYear: 2012,
+	director: "Joss Whedon"
+};
+
+var greeting = function (){
+console.log(avengersMovie);
+};
+greeting();
